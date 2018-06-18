a7bd3e2b26d712bd054522593175ece182b65e16
Tom Carrio <Thomas.Carrio@Ally.com>
2018-06-18 15:59:40 -0400

    Added CRD capability.

--- src/main/java/com/el/jokes_mvc/controllers/JokesController.java
+++ src/main/java/com/el/jokes_mvc/controllers/JokesController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.el.jokes_mvc.models.Joke;
@@ -21,12 +23,39 @@ public class JokesController {
 	}
 	
 	@GetMapping("/")
-	public ModelAndView calculator() {
+	public ModelAndView index() {
+		return new ModelAndView("redirect:/read");
+	}
+	
+	@GetMapping("/read")
+	public ModelAndView readJokes() {
 		ModelAndView mv = new ModelAndView();
 		List<Joke> jokes = jokeRepository.findAll();
 		mv.addObject("jokes", jokes);
 		mv.setViewName("read");
 		return mv;
+	}
+	
+	@GetMapping("/create")
+	public ModelAndView createJokePage() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("create");
+		return mv;
+	}
+	
+	@PostMapping("/create")
+	public ModelAndView createJokeModel(
+			@RequestParam(value="joke") String joke,
+			@RequestParam(value="punchline") String punchline,
+			@RequestParam(value="rating") int rating) {
+		jokeRepository.save(new Joke(joke, punchline, rating));
+		return readJokes();
+	}
+	
+	@GetMapping("/delete")
+	public ModelAndView createJokeModel(@RequestParam(value="id") int id) {
+		jokeRepository.delete(id);
+		return readJokes();
 	}
 
 }
